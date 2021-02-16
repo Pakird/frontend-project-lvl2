eec6abe8dd0f89e90238851f4941b67183aa8497
Kirill Pastukhov <Pastukhov475@icloud.com>
2021-02-16 18:24:24 +0300

    add commander

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
-console.log('hi');
+import commander from 'commander';
+const program = new commander.Command();
+
+program.version('0.0.1');
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .addOption(new commander.Option('-s, --secret').hideHelp())
+
+program.parse(process.argv);
+console.log('Options: ', program.opts());
